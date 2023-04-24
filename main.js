36c5a607c07c52eb29ee93591e65cec3a43e43cf
winninggodspower <winninggodspower@gmail.com>
2023-04-24 13:51:32 +0100

    made changes

--- main.js
+++ main.js
@@ -2,3 +2,19 @@
 document.getElementById("read-more-button").addEventListener('click', (e) => {
     e.target.style.display = "none";
 })
+
+const navBar = document.querySelector("nav")
+const navbarToggler = document.getElementById("navbar-toggler-button");
+const collapsable = document.getElementById("navbarsExample11");
+
+
+navbarToggler.addEventListener("click", ()=>{
+	if (navbarToggler.classList.contains("collapsed")){
+		navBar.style.height = "100px";
+		collapsable.style.paddingBottom = "0px";
+	} else{
+		navBar.style.height = "fit-content";
+		collapsable.style.paddingBottom = "30px";
+	}
+})
+
